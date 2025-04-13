186603a053608d9bd2f4a2c7e406fb811822183d
Jack Lin <jacklin@cybersoft4u.com>
2025-04-13 05:09:30 -0700

    Disable portals/ally/mirror until they’re introduced

--- Assets/Scripts/TutorialScripts/PortalGunCollectible.cs
+++ Assets/Scripts/TutorialScripts/PortalGunCollectible.cs
@@ -8,16 +8,33 @@ public class PortalGunCollectible : MonoBehaviour
 {
     public GameObject player;
     public PopupManager popupManager;
+
+    private PortalManager portalManager;
+
+    void Start()
+    {
+        portalManager = FindObjectOfType<PortalManager>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))// && SceneManager.GetActiveScene().name == "tutorial")
+        if (other.CompareTag("Player"))
         {
             LineRenderer line = other.GetComponent<LineRenderer>();
-            if(line != null){
-                line.enabled = true;
+            if (SceneManager.GetActiveScene().name == "tutorial")
+            {
+                portalManager.CanUsePortal = true;
+                if (line != null)
+                {
+                    line.enabled = true;
+                }
+            }
+            else if (SceneManager.GetActiveScene().name == "allyTutorial")
+            {
+                portalManager.CanUseCage = true;
             }
 
-            if(popupManager != null)
+            if (popupManager != null)
             {
                 popupManager.ShowPopupText();
             }
