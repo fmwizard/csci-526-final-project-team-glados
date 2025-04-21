6bb45db130fbe9764135d3bb0033eecfbb7eba02
Mina Sung <156390419+minasung7@users.noreply.github.com>
2025-04-21 15:57:35 -0700

    Add hintzone tag for laser to ignore hintzone collider; Adjust lasercontroller.cs accordingly; add hintzone tag to hintzone prefab

--- Assets/Scripts/Laser/LaserController.cs
+++ Assets/Scripts/Laser/LaserController.cs
@@ -127,7 +127,7 @@ public class LaserController : MonoBehaviour
                 }
 
                 // just continue if we hit a portal
-                if (hit.collider.CompareTag("Portal") || hit.collider.CompareTag("Cage"))
+                if (hit.collider.CompareTag("Portal") || hit.collider.CompareTag("Cage") || hit.collider.CompareTag("HintZone"))
                 {
                     start = hit.point + direction * 0.05f; // Offset to prevent self-hits
                     remainingDistance -= hit.distance;
